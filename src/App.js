5a93d0003463bd667575f1a7ac295e4e4d594bb3
Mrutyunjay Madarimath <mrutyunjay69@gmail.com>
2024-12-03 23:59:39 +0530

    movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from "react";
+import { useEffect } from "react";
 import './App.css';
+import search from "./search.svg"
+//API key 87a9f052
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'https://www.omdbapi.com?apikey=87a9f052'
+const App = () => {
+
+    const searchMovies = async (title) => {
+      const response = await fetch(`${API_URL}&s=${title}`)
+      const data = await response.json()
+
+      console.log(data)
+    }
+
+    useEffect(() => {
+      searchMovies('Avengers')
+    }, [])
+
+    return (
+    <h1>Movies</h1>
+)
 }
 
-export default App;
+export default App;     
